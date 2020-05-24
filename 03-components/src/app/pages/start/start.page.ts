f40ab14ea132151a96fe9288fad563f18aee7802
yvnpaz <yppousa@esei.uvigo.es>
2020-05-24 19:59:24 +0200

    Icon start

--- 03-components/src/app/pages/start/start.page.ts
+++ 03-components/src/app/pages/start/start.page.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartPage implements OnInit {
 
+  components: ElementComponent[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface ElementComponent {
+
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
